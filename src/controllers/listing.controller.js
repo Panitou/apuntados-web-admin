bf1bb24804b29f2f8dc80773447be68b2a314ae2
Panitou <joaco0mr4@gmail.com>
2024-07-09 01:19:50 -0500

    eliminacion de rutas innecesarias

--- src/controllers/listing.controller.js
+++ src/controllers/listing.controller.js
@@ -1,27 +1,5 @@
 import Listing from "../models/listing.model.js";
 
-// Crear un apunte
-export const addListing = async (req, res) => {
-  const { name, description, course, semester, price, imageUrls, userRef } =
-    req.body;
-
-  try {
-    const newListing = new Listing({
-      name,
-      description,
-      course,
-      semester,
-      price,
-      imageUrls,
-      userRef,
-    });
-    await newListing.save();
-    res.status(201).json(newListing);
-  } catch (error) {
-    res.status(500).json({ message: "Error adding listing" });
-  }
-};
-
 // Obtener todos los apuntes
 export const getListings = async (req, res) => {
   try {
@@ -29,36 +7,6 @@ export const getListings = async (req, res) => {
     res.status(200).json(listings);
   } catch (error) {
     res.status(500).json({ message: "Error fetching listings" });
-  }
-};
-
-// Obtener un apunte por ID
-export const getListingById = async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const listing = await Listing.findById(id);
-    if (!listing) return res.status(404).json({ message: "Listing not found" });
-    res.status(200).json(listing);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching listing" });
-  }
-};
-
-// Actualizar un apunte por ID
-export const updateListingById = async (req, res) => {
-  const { id } = req.params;
-  const updateData = req.body;
-
-  try {
-    const updatedListing = await Listing.findByIdAndUpdate(id, updateData, {
-      new: true,
-    });
-    if (!updatedListing)
-      return res.status(404).json({ message: "Listing not found" });
-    res.status(200).json(updatedListing);
-  } catch (error) {
-    res.status(500).json({ message: "Error updating listing" });
   }
 };
 
--- src/routes/listing.route.js
+++ src/routes/listing.route.js
@@ -1,19 +1,13 @@
 import { Router } from "express";
 import {
-  addListing,
   getListings,
-  getListingById,
-  updateListingById,
   deleteListingById,
 } from "../controllers/listing.controller.js";
 
 const router = Router();
 
 // Rutas para apuntes
-router.post("/listings", addListing);
 router.get("/listings", getListings);
-router.get("/listings/:id", getListingById);
-router.put("/listings/:id", updateListingById);
 router.delete("/listings/:id", deleteListingById);
 
 export default router;
